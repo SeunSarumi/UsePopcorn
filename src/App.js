88a3a2504c2398c33e9bbde3df1bcfc2854e8521
SeunSarumi <seunsarumi@gmail.com>
2024-01-08 13:19:38 +0100

    loading state and component

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ const KEY = "74e006a1";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [IsLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   // useEffect(function () {
@@ -68,12 +69,13 @@ export default function App() {
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
-      console.log(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -86,9 +88,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{IsLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
@@ -96,6 +96,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 // NAVIGATION
