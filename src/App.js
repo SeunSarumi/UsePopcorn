4075f6d64dc03ff2e4655fe8d1103c7ebfb55cde
SeunSarumi <seunsarumi@gmail.com>
2024-01-09 21:21:10 +0100

    abort controller

--- src/App.js
+++ src/App.js
@@ -104,12 +104,15 @@ export default function App() {
   // Function to fetch the movies from APi
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -118,8 +121,11 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not Found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -131,6 +137,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
